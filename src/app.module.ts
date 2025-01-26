a198c89755dacdc0f97699129c60c532357491a9
Hachemi <hachemi@protonmail.com>
2025-01-26 19:21:43 +0100

    feat(app): configure TypeORM et importe le module Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { UsersModule } from './modules/users/users.module';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [],
 })
